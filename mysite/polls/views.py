8723e5e597ddd34ea81b941f68681e00f151cde9
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-13 23:14:37 -0700

    Add another view and route for 'wow' inside the polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def wow(request):
+    return HttpResponse("Wow, it works. You're at the polls Wow page.")
