d99440494180080549960a54214e489c4867fff5
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-14 18:08:35 -0700

    Add a few simple, public interfacing views. Wire these views into polls.urls.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,3 +7,13 @@ def index(request):
 
 def wow(request):
     return HttpResponse("Wow, it works. You're at the polls Wow page.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
