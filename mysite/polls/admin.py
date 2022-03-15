a4bc4dbb6461cae9de0947ae22a7885687cecb94
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-14 17:52:26 -0700

    Register Question model with the admin app

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# I.e., tell the admin that `Question` objects have an admin interface
+
+from .models import Question
+
+admin.site.register(Question)
