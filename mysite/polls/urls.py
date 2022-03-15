08e9ddc397ce95ba5fefefb7a4b1990dc71b0269
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-14 23:50:50 -0700

    Use the generic views system; convert URLconf and views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,15 +9,24 @@ from . import views
 # A: Add namespaces to your URLconf. In the polls/urls.py file, add an app_name
 # to set the application namespace:
 app_name = 'polls'
+
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    # The 'name' value as called by the {% url %} template tag.
-    # And we added the word 'specifics'
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results
-    path('<int:question_id>/results', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote', views.vote, name='vote'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# This is the hard way. We went through this for learning.
+# urlpatterns = [
+#     # ex: /polls/
+#     path('', views.index, name='index'),
+#     # ex: /polls/5/
+#     # The 'name' value as called by the {% url %} template tag.
+#     # And we added the word 'specifics'
+#     path('<int:question_id>/', views.detail, name='detail'),
+#     # ex: /polls/5/results
+#     path('<int:question_id>/results', views.results, name='results'),
+#     # ex: /polls/5/vote/
+#     path('<int:question_id>/vote', views.vote, name='vote'),
+# ]
