b75ec84b2d512e4067ad966b7e43d372b2b3f48d
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-14 18:24:18 -0700

    Use Django's template system to create a template that the view can use.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 # from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -15,10 +16,16 @@ from .models import Question
 # HttpResponse. Or an exception.
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
     # return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def wow(request):
     return HttpResponse("Wow, it works. You're at the polls Wow page.")
