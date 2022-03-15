52914a506d95868ec6fbcedc861c6a550bcb74fb
Carlos Moreno <soyelmoreno@gmail.com>
2022-03-14 21:29:54 -0700

    Create a detail view. Raise a Http404 exception if a question doesn't exist

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -41,7 +42,17 @@ def wow(request):
     return HttpResponse("Wow, it works. You're at the polls Wow page.")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # Version 1: very simple
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    # Version 2:
+    # Use the render shortcut to render the template with the context
+    # And raise an exception if that question id doesn't exist.
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
